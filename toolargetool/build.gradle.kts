c9b6eee89c6596ef65411079067093b02a002469
Ranjith <108326704+ranjith-zuper@users.noreply.github.com>
2024-12-02 20:40:32 +0530

    Temporarily remove POM and signing setup for publishing on Jitpack

--- toolargetool/build.gradle.kts
+++ toolargetool/build.gradle.kts
@@ -20,6 +20,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
@@ -31,85 +37,98 @@ dependencies {
     androidTestImplementation(libs.mockito.kotlin)
 }
 
-val sourcesJar by tasks.creating(Jar::class) {
-    from(android.sourceSets["main"].java.srcDirs)
-    archiveClassifier.set("sources")
-}
-
-val javadocJar by tasks.creating(Jar::class) {
-    // There are no docs here
-    archiveClassifier.set("javadoc")
-}
-
-afterEvaluate {
-    publishing {
-        val isSnapshot = project.hasProperty("snapshot")
-
-        publications {
-            val release by creating(MavenPublication::class) {
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.gu.android"
+            artifactId = "toolargetool"
+            version = "0.3.2"
+            afterEvaluate {
                 from(components["release"])
-                artifact(sourcesJar)
-                artifact(javadocJar)
-
-                groupId = "com.gu.android"
-                artifactId = "toolargetool"
-                version = "0.3.2" + if (isSnapshot) "-SNAPSHOT" else ""
-
-                pom {
-                    name.set("toolargetool")
-                    description.set("A tool for debugging TransactionTooLargeException on Android")
-                    url.set("https://github.com/guardian/toolargetool")
-                    licenses {
-                        license {
-                            name.set("The MIT License (MIT)")
-                            url.set("https://opensource.org/licenses/MIT")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:git://github.com/guardian/toolargetool.git")
-                        developerConnection.set("scm:git:ssh://github.com/guardian/toolargetool.git")
-                        url.set("https://github.com/guardian/toolargetool")
-
-                    }
-                    developers {
-                        developer {
-                            id.set("maxspencer")
-                            name.set("Max Spencer")
-                            email.set("max.spencer@guardian.co.uk")
-                            url.set("https://github.com/maxspencer")
-                            organization.set("The Guardian")
-                            organizationUrl.set("https://theguardian.com")
-                        }
-                    }
-                }
             }
         }
+    }
+}
 
-//        repositories {
-//            maven {
-//                name = "Sonatype"
-//                url = uri("https://oss.sonatype.org" + if(isSnapshot) {
-//                    "/content/repositories/snapshots/"
-//                } else {
-//                    "/service/local/staging/deploy/maven2/"
-//                })
-//                credentials {
-//                    username = properties["ossrhUsername"] as? String
-//                    password = properties["ossrhPassword"] as? String
+//val sourcesJar by tasks.creating(Jar::class) {
+//    from(android.sourceSets["main"].java.srcDirs)
+//    archiveClassifier.set("sources")
+//}
+//
+//val javadocJar by tasks.creating(Jar::class) {
+//    // There are no docs here
+//    archiveClassifier.set("javadoc")
+//}
+
+//afterEvaluate {
+//    publishing {
+//        val isSnapshot = project.hasProperty("snapshot")
+//
+//        publications {
+//            val release by creating(MavenPublication::class) {
+//                from(components["release"])
+//                artifact(sourcesJar)
+//                artifact(javadocJar)
+//
+//                groupId = "com.gu.android"
+//                artifactId = "toolargetool"
+//                version = "0.3.2" + if (isSnapshot) "-SNAPSHOT" else ""
+//
+//                pom {
+//                    name.set("toolargetool")
+//                    description.set("A tool for debugging TransactionTooLargeException on Android")
+//                    url.set("https://github.com/guardian/toolargetool")
+//                    licenses {
+//                        license {
+//                            name.set("The MIT License (MIT)")
+//                            url.set("https://opensource.org/licenses/MIT")
+//                        }
+//                    }
+//                    scm {
+//                        connection.set("scm:git:git://github.com/guardian/toolargetool.git")
+//                        developerConnection.set("scm:git:ssh://github.com/guardian/toolargetool.git")
+//                        url.set("https://github.com/guardian/toolargetool")
+//
+//                    }
+//                    developers {
+//                        developer {
+//                            id.set("maxspencer")
+//                            name.set("Max Spencer")
+//                            email.set("max.spencer@guardian.co.uk")
+//                            url.set("https://github.com/maxspencer")
+//                            organization.set("The Guardian")
+//                            organizationUrl.set("https://theguardian.com")
+//                        }
+//                    }
 //                }
 //            }
 //        }
-    }
-
-    signing {
-        if (project.hasProperty("useInMemoryPgpKeys")) {
-            val signingKey: String? by project
-            val signingKeyPassword: String? by project
-            if (signingKey.isNullOrEmpty() || signingKeyPassword.isNullOrEmpty()) {
-                logger.log(LogLevel.ERROR, "The useInMemoryPgpKeys property was set but signingKey and/or signingKeyPassword are missing")
-            }
-            useInMemoryPgpKeys(signingKey, signingKeyPassword)
-        }
-        sign(publishing.publications["release"])
-    }
-}
+//
+////        repositories {
+////            maven {
+////                name = "Sonatype"
+////                url = uri("https://oss.sonatype.org" + if(isSnapshot) {
+////                    "/content/repositories/snapshots/"
+////                } else {
+////                    "/service/local/staging/deploy/maven2/"
+////                })
+////                credentials {
+////                    username = properties["ossrhUsername"] as? String
+////                    password = properties["ossrhPassword"] as? String
+////                }
+////            }
+////        }
+//    }
+//
+//    signing {
+//        if (project.hasProperty("useInMemoryPgpKeys")) {
+//            val signingKey: String? by project
+//            val signingKeyPassword: String? by project
+//            if (signingKey.isNullOrEmpty() || signingKeyPassword.isNullOrEmpty()) {
+//                logger.log(LogLevel.ERROR, "The useInMemoryPgpKeys property was set but signingKey and/or signingKeyPassword are missing")
+//            }
+//            useInMemoryPgpKeys(signingKey, signingKeyPassword)
+//        }
+//        sign(publishing.publications["release"])
+//    }
+//}
